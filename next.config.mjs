c0b4df81e76836002ce79b4dcb0e93467b1edda5
Husnain0120 <muhammadhusnainrashid.bs@gmail.com>
2025-07-09 16:11:31 +0500

    refactor: streamline PWA and Next.js configuration in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,21 @@
 import withPWA from "next-pwa";
 
-// Next.js configuration
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
+  },
+};
+
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+  eslint: {
+    ignoreDuringBuilds: true,
   },
   images: {
     domains: ["img.freepik.com"],
     unoptimized: true,
   },
-};
-
-// PWA configuration
-const pwaConfig = {
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-};
-
-// Export the combined configuration
-export default withPWA({
-  ...nextConfig,
-  pwa: pwaConfig,
-});
+})(nextConfig);
